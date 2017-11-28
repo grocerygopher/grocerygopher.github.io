0e9e9276d2e7073dfc3572ef8cf84a475fea8742
grocerygopher <33602213+grocerygopher@users.noreply.github.com>
2017-11-28 12:05:41 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,9 +12,33 @@ function showContent(sPage, sTitle) {
         document.getElementById('contentBody').innerHTML= this.responseText;};
 
     xhr.send();
+
+    // Add this script.js reference to cDocs.html
+    if (sPage =="cDocs.html"){
+        var jsFile=document.createElement("script");
+            jsFile.setAttribute("type","text/javascript");
+            jsFile.setAttribute("src", "script.js");
+        document.body.appendChild(jsFile);
+        filesadded+="script.js";
+        alert("script.js added");
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {showContent('cHome','Grocery Gopher Home');}, false);
+
+function shopping() {
+
+    document.getElementById("phone2").src="image/tpx.png";
+    document.getElementById("phone3").src="image/tpx.png";
+
+}
+
+function pantry() {
+
+    document.getElementById("phone2").src="image/pList.png";
+    document.getElementById("phone3").src="image/less_than.png";
+
+}
 
 
 
